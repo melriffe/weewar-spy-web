f293cefd477adcb78cc9413e987b709f02b72538
Mel Riffe <mriffe@gmail.com>
2008-12-18 00:45:20 -0500

    moving inline tempates into separate files

--- spyweb.rb
+++ spyweb.rb
@@ -37,7 +37,7 @@ get '/' do
   haml :index
 end
 
-get '/styles.css' do
+get '/stylesheets/styles.css' do
   content_type 'text/css', :charset => 'utf-8'
   sass :stylesheet
 end
@@ -51,108 +51,3 @@ get '/game/:id' do
   end
   haml :game
 end
-
-use_in_file_templates!
-
-__END__
-
-@@ layout
-!!!
-%html
-  %head
-    %title= Title
-    %link{:rel => 'stylesheet', :href => '/styles.css', :type => 'text/css'}
-    %meta{'http-equiv' => 'Content-Type', :content => 'text/html'}
-    /
-      %meta{'http-equiv' => 'Refresh', :content => '10; URL=http://localhost:4567/'}
-
-  %body
-    #container
-      #header
-        %h1= Title
-      #main
-        %h2 Spy Details
-        #spy-details
-          %p== Player: #{SpyConfig['username']}
-          %p== Server: #{SpyConfig['server']}
-        = yield
-
-
-@@ index
-#games
-  %h2 Games
-  %ul
-    - @games.each do |id, name|
-      %li.game{:id => id}
-        %a{:href => "/game/#{id}"}= name
-
-
-@@ game
-#menu
-  %a{:href => '/'} Headquarters
-  %a{:href => "#{@game.url}", :target => "_blank"}= @game_button_label
-%h2= @game.name
-#game
-  %pre= @report
-
-
-@@ stylesheet
-html
-  :margin 0
-  :padding 0
-
-body
-  :font
-    :family "Helvetica", "Arial", sans-serif
-  :margin-left 2.6em
-  :margin-top 1.8em
-  :margin-bottom 1em
-  :margin-right 1em
-
-#container
-  :margin 0 auto
-  :width 700px
-
-#header h1
-  :margin 10px 0
-  :text-align center
-
-#main h2
-  :margin 0
-
-#main
-  #spy-details p
-    :margin 5px 0
-
-#menu
-  :margin 10px 0
-  :padding 10px 0
-  a
-    :background-color #ccc
-    :color #333
-    :text-decoration none
-    :padding 0.5em
-    &:hover
-      :background-color #333
-      :color #ccc
-
-#games h2
-  :margin-top 1em
-#games ul
-  :list-style-type square
-  :margin 5px 0
-  :padding-left 1em
-#games li
-  :padding 5px
-#games a
-  :border-bottom 1px dashed #333
-  :color #333
-  :text-decoration none
-  &:hover
-    :border-bottom 1px solid #333
-
-#game pre
-  :background-color #eee
-  :border 1px dashed black
-  :margin 0.5em
-  :padding 0.75em
